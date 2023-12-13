380a685eb07191fd3d8220ed0c3ba3b429d16e40
solen003 <olender.seb@gmail.com>
2023-12-13 10:10:29 -0500

    Add bug fixes

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 from modules import functions_for_app1
 import PySimpleGUI as psg
+import time
 
+psg.theme("DarkPurple4")
+
+clock = psg.Text("", key="clock")
 label = psg.Text("Type in a to-do")
 input_box = psg.InputText(tooltip="Enter todo", key="input_key") #output when ADD pressed: ('Add', {'todo_key': 'hello'})
 add_button = psg.Button("Add")
@@ -10,7 +14,8 @@ delete_button = psg.Button("Delete")
 exit_button = psg.Button("Exit")
 
 window = psg.Window("My To-Do App",
-                    layout=[[label],
+                    layout=[[clock],
+                            [label],
                             [input_box, add_button],
                             [list_box, edit_button, delete_button],
                             [exit_button]
@@ -18,7 +23,9 @@ window = psg.Window("My To-Do App",
                     font=('Helvetica', 10))
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=500)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     print('Event is:',event)
     print(values)
 
@@ -32,28 +39,37 @@ while True:
         window['list_box_key'].update(values=todos)
 
     elif event == "Edit":
-        todos = functions_for_app1.get_todos()
+        try:
+            todos = functions_for_app1.get_todos()
 
-        new_todo = values['input_key']
-        index = todos.index(values['list_box_key'][0])
-        todos.__setitem__(index, new_todo)
+            new_todo = values['input_key']
+            index = todos.index(values['list_box_key'][0])
+            todos.__setitem__(index, new_todo)
 
-        functions_for_app1.write_todos(todos)
-        window['list_box_key'].update(values=todos)
+            functions_for_app1.write_todos(todos)
+            window['list_box_key'].update(values=todos)
+
+        except IndexError:
+            psg.popup("Please select an item first to edit")
+            continue
 
     elif event == "list_box_key":
         # Update the INPUT box to the value selected from the listbox
         window['input_key'].update(value=values['list_box_key'][0])
 
     elif event == "Delete":
-        todos = functions_for_app1.get_todos()
-        todo_to_delete = values['list_box_key'][0]
-        todos.remove(todo_to_delete)
+        try:
+            todos = functions_for_app1.get_todos()
+            todo_to_delete = values['list_box_key'][0]
+            todos.remove(todo_to_delete)
 
-        functions_for_app1.write_todos(todos)
-        window['list_box_key'].update(values=todos)
-        window['input_key'].update(value="")
+            functions_for_app1.write_todos(todos)
+            window['list_box_key'].update(values=todos)
+            window['input_key'].update(value="")
 
+        except IndexError:
+            psg.popup("Please select an item first to edit")
+            continue
     elif event == "Exit":
         break
     elif event == psg.WIN_CLOSED:
