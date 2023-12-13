a728bb16eda27c498262c7a6d7ee9bcbb91a13e4
solen003 <olender.seb@gmail.com>
2023-12-13 08:43:03 -0500

    Add button implemented

--- gui.py
+++ gui.py
@@ -6,11 +6,14 @@ input_box = psg.InputText(tooltip="Enter todo", key="input_key") #output when AD
 add_button = psg.Button("Add")
 list_box = psg.Listbox(values=functions_for_app1.get_todos(), key='list_box_key', enable_events=True, size=[44,10])
 edit_button = psg.Button("Edit")
+delete_button = psg.Button("Delete")
+exit_button = psg.Button("Exit")
 
 window = psg.Window("My To-Do App",
                     layout=[[label],
                             [input_box, add_button],
-                            [list_box, edit_button]
+                            [list_box, edit_button, delete_button],
+                            [exit_button]
                             ],
                     font=('Helvetica', 10))
 
@@ -21,30 +24,38 @@ while True:
 
     if event == "Add":
         todos = functions_for_app1.get_todos()
-        todo = values['input_key'] + "\n"
-        todos.append(todo)
+
+        new_todo = values['input_key'] + "\n"
+        todos.append(new_todo)
 
         functions_for_app1.write_todos(todos)
-
         window['list_box_key'].update(values=todos)
 
     elif event == "Edit":
         todos = functions_for_app1.get_todos()
 
-        edited_todo = values['list_box_key'][0]
-        new_todo = values['input_key'] + "\n"
-        print("new todo is", new_todo)
-        index = todos.index(edited_todo)
+        new_todo = values['input_key']
+        index = todos.index(values['list_box_key'][0])
         todos.__setitem__(index, new_todo)
 
         functions_for_app1.write_todos(todos)
-
         window['list_box_key'].update(values=todos)
 
     elif event == "list_box_key":
         # Update the INPUT box to the value selected from the listbox
         window['input_key'].update(value=values['list_box_key'][0])
 
+    elif event == "Delete":
+        todos = functions_for_app1.get_todos()
+        todo_to_delete = values['list_box_key'][0]
+        todos.remove(todo_to_delete)
+
+        functions_for_app1.write_todos(todos)
+        window['list_box_key'].update(values=todos)
+        window['input_key'].update(value="")
+
+    elif event == "Exit":
+        break
     elif event == psg.WIN_CLOSED:
         break
         #exit()
